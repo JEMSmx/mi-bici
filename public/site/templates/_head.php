f99303d7461b9d807d8d1a0a8f3e8b8619a4ab31
Jesa Navarro <jesaa@me.com>
2017-04-18 14:10:23 -0500

    Wip 1 (#2)
    * Compress images
    
    * Minor changes

--- public/site/templates/_head.php
+++ public/site/templates/_head.php
@@ -11,11 +11,6 @@
   <link href="<?php echo $config->urls->templates; ?>static/455375-1485995688/styles/main.css" rel="stylesheet">
   <link rel="apple-touch-icon" href="<?php echo $config->urls->templates; ?>static/455375-1485995688/images/favicon-180x180.png">
   <link rel="shortcut icon" href="<?php echo $config->urls->templates; ?>static/455375-1485995688/images/favicon-64x64.png">
-  <!-- Start of bktbp Zendesk Widget script -->
-<script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(e){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("https://assets.zendesk.com/embeddable_framework/main.js","bktbp.zendesk.com");
-/*]]>*/</script>
-<!-- End of bktbp Zendesk Widget script -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
 </head>
 
 <body class="<?php echo ($page->template=='home') ? 'home':k::page_name(); ?> >">
